ee9171ae9cf769f8de3f4cb929df6b2656cad516
Narukula Chiranjeevi <nchiranjeevi987@gmail.com>
2023-10-26 11:29:18 +0530

    Adding permission to views

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -4,6 +4,10 @@ from .serializers import SubjectSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
+
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -14,8 +18,10 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
     def post(self,request,pk,format=None):
-        course = Course.get_object_or_404(pk=pk)
+        course = get_object_or_404(Course,pk=pk)
         course.students.add(request.user)
         return Response({'enrolled':True})
 
